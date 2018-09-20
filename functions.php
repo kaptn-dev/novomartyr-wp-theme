c0aa77758f251d0b99dba54f909dd09ed79c7cce
vikapitoshka <kapitonenko.vick@ya.ru>
2018-09-20 18:36:35 +0300

    Очистка

--- functions.php
+++ functions.php
@@ -1,21 +1,6 @@
 <?php
-#add_filter('excerpt_more', 'new_excerpt_more');
-#function new_excerpt_more($more) {
-#    global $post;
-#    return '<a href="'. get_permalink($post->ID) . '">Читать дальше...</a>';
-#};
 
 add_theme_support('post-thumbnails');
-
-/* функции подключения файлов */
-
-#function style_script_load() {
-#	wp_enqueue_script( 'bootstrap.min', get_template_directory_uri() . '/js/bootstrap.min.js');
-#	wp_enqueue_style('bootstrap.min', get_template_directory_uri() . '/css/bootstrap.min.css');
-#	wp_enqueue_style('style', get_template_directory_uri() . '/style.css');
-#};
-	
-#add_action( 'wp_enqueue_scripts', 'style_script_load' );
 
 /* Header menu */
 
@@ -34,7 +19,5 @@ function my_navigation_template( $template, $class ){
 	</nav>    
 	';
 }
-// удаление category из url
-//add_filter('category_link', create_function('$a', 'return str_replace("category/", "", $a);'), 9999);
 
 ?>
