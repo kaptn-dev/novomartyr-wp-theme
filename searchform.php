33da8037f94644d94d5b7b22f00296511aa5dade
vikapitoshka <kapitonenko.vick@ya.ru>
2018-09-20 18:46:20 +0300

    Форма поиска в сайдбаре

---
+++ searchform.php
@@ -0,0 +1,6 @@
+<div class="search">
+  <form role="search" method="get" action="<?php echo home_url( '/' ); ?>" class="search-form">
+    <input type="search" class="search-form__input" value="<?php echo get_search_query() ?>" name="s">
+    <button class="search-form__submit">Поиск</button>
+  </form>
+</div>
--- sidebar.php
+++ sidebar.php
@@ -1,18 +1,10 @@
 <!-- Дополнительное меню -->
 <div class="col-sm-12 col-md-3 blog-sidebar">
   <div class="sidebar-module">
+    <?php get_search_form(); ?>
+  </div>
+  <div class="sidebar-module">
     <div class="btn-group-vertical panel">
-      <!--button type="button" class="btn btn-info btn-block">Благодарим:</button>
-      <ul class="list-unstyled">
-        <li></li>
-        <li></li>
-        <div class="collapse" id="collapseExample">
-          <li></li>
-          <li></li>
-          <li></li>
-        </div>
-        <a data-toggle="collapse" href="#collapseExample" aria-expanded="false" aria-controls="collapseExample">Скрыть/показать</a>
-      </ul-->
       <a role="button" class="btn btn-info btn-block" href="/category/prop">Проповеди</a>
       <a role="button" class="btn btn-info btn-block" href="/proj">Строительство храма</a>
       <a role="button" class="btn btn-info btn-block" href="/help">Хочу помочь</a>
